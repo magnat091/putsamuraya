3653f694be14f1ff8bb6f2a30c54ad84a0b30fb7
agalaev <agalaev@kameleoon.com>
2022-04-21 01:17:00 +0300

    pipec

---
+++ src/components/profile.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Profile = () =>{
+    return(
+        <div className={'content'}>
+            <div>
+                <img src="https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg" alt=""/>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    <div>post1</div>
+                    <div>post2</div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Profile;
